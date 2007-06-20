ae4175e5c1ecfe8ff42ebe2d6b86f98a61fadd5f
njl <devnull@localhost>
2007-06-19 20:37:31 -0400

    [svn r8] -Initial checkin

---
+++ src/gui/EncounterMessageBox.py
@@ -0,0 +1,19 @@
+#
+# EncounterMessageBox.py
+# Nick Loadholtes
+# June 19, 2007
+#
+# This class is intended to be the display for
+# encounters in the brass engine.
+#
+
+class EncounterMessageBox:
+    def __init__(self):
+        print "Creating an EncounterMessageBox"
+
+    def display(self, text):
+        '''This message will take the input text and display it
+        to the EncounterMessageBox'''
+        print text
+
+
