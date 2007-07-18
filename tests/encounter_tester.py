031a2a3144b4bed9d1b86d2afbb5ea33a24f9283
njl <devnull@localhost>
2007-07-17 20:07:55 -0400

    [svn r34] -Initial checkin -Testing encounters of a non-combatitve nature

---
+++ tests/encounter_tester.py
@@ -0,0 +1,32 @@
+#
+# encounter_tester.py
+# July 17, 2007
+# Nick Loadholtes
+#
+# A tester to try out non-fighting encounters
+#
+from EncounterManager import *
+
+def startenc(encounter):
+    print "Starting encounter:"
+    print 'Preamble: ', encounter['preamble']
+    x = 1
+    while(x):
+        topic = encounter['conversation'][x]
+        print "\t",topic[0]
+        if(len(topic) > 1):
+            x = eval(topic[1])
+        
+
+
+if __name__ == "__main__":
+    encounters = {}
+    execfile('../src/encounter.py', globals(), encounters)
+    print encounters['encounter']['title']
+    startenc(encounters['encounter'])
+#    target = raw_input()
+#    if target == '1':
+
+
+
+
