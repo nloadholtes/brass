443722c6010de81e50b395afe3d1c94b38993350
njl <devnull@localhost>
2007-07-17 20:06:52 -0400

    [svn r33] -Initial checkin -This is a prototype of what the encounter data structure should look like. Actual encounters probably need to be in their own directory (since they will be story specific)

---
+++ src/Encounter.py
@@ -0,0 +1,27 @@
+#
+# encounter.py
+# July 17, 2007
+# Nick Loadholtes
+#
+# The data structure for an encounter
+#
+
+
+
+encounter = {
+    'title' : 'Mike talking to Tom',
+    'preamble' : 'Mike are you ok?',
+    'conversation' : [(),
+                      ('Are You ok?', 'yes_or_no(2,3)'),
+                      ('Good, I\'m glad to hear that', '4'),
+                      ('What? Toughen up you crybaby...', 'None'),
+                      ('There was a bad earthquake!', 'pause(5)'),
+                      ('I need you to go check on my neighbors, make sure they\
+                      are ok.', 'None')
+
+                      ],
+    'talking' : True
+    
+    }
+
+
