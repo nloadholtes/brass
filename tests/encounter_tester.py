8a07daa8b5fa2c3b9c74ac3f96d92e5d16f4ed78
njl <devnull@localhost>
2007-08-19 20:30:53 -0400

    [svn r93] -Refactoring to use the new Encounter Engine.startEncounter()

--- tests/encounter_tester.py
+++ tests/encounter_tester.py
@@ -7,15 +7,15 @@
 #
 from EncounterManager import *
 
-def startenc(encounter):
-    print "Starting encounter:"
-    print 'Preamble: ', encounter['preamble']
-    x = 1
-    while(x):
-        topic = encounter['conversation'][x]
-        print "\t",topic[0]
-        if(len(topic) > 1):
-            x = eval(topic[1])
+#def startenc(encounter):
+#    print "Starting encounter:"
+#    print 'Preamble: ', encounter['preamble']
+#    x = 1
+#    while(x):
+#        topic = encounter['conversation'][x]
+#        print "\t",topic[0]
+#        if(len(topic) > 1):
+#            x = eval(topic[1])
         
 
 
@@ -23,7 +23,8 @@ if __name__ == "__main__":
     encounters = {}
     execfile('../src/encounter.py', globals(), encounters)
     print encounters['encounter']['title']
-    startenc(encounters['encounter'])
+    encmgr = EncounterEngine(createBadGuys(1, 1, 0))
+    encmgr.startEncounter(encounters['encounter'])
 #    target = raw_input()
 #    if target == '1':
 
