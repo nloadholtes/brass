d73e0842c8ba29413219e81201390ce40f9ebe93
njl <devnull@localhost>
2007-08-21 21:02:39 -0400

    [svn r95] -Added a test encounter to see what we can test and find

--- src/Encounter.py
+++ src/Encounter.py
@@ -20,4 +20,16 @@ encounter = {
                       ],
     }
 
-
+test_encounter = {
+	'title' : 'Our Test encounter',
+	'preamble' : 'What\'s up?',
+	'conversation' : [(),
+					  ('Are You ok?', 'yes_or_no(2,3)'),
+					  ('Good, I\'m glad to hear that', '4'),
+					  ('What? Toughen up you crybaby...', 'None'),
+					  ('There was a bad earthquake!', 'pause(5)'),
+					  ('I need you to go check on my neighbors, make sure they are ok.', 'None')
+					  ],
+				
+				
+				}
