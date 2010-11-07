17e163eac6b8420944c827efe52acae47cc83871
Nick <Nick Loadholtes nick@ironboundsoftware.com>
2010-11-07 01:25:42 -0500

    Fixing formatting, fixing eclipse project file

--- src/game.py
+++ src/game.py
@@ -4,36 +4,36 @@
 
 from CPUSpinnerController import *
 from EventMngr import *
-from Input import * 
+from Input import *
 from tileengine import *
-import sys
 import gui.GUIToolkit
+import sys
 
 
 
-if __name__== "__main__":
-       #
-       #Read in the game file how can you tell?
-       values = {}
-       print sys.argv
-       if(len(sys.argv) > 1):
-              print "Loading game file"
-              execfile(sys.argv[1], globals(), values)
-       else :
-              raise Exception, "Can't run with out a game file"
-
-       gamedata = values.get('gamedata')
-       evtmngr = EventManager()
-       gtk = GUIToolkit()
-       keybd = Input(evtmngr, gtk)
-       spinner = CPUSpinnerController(evtmngr)
-       te = TileEngine(evtmngr, gamedata, gtk)
-       #game = Game( evManager )
-
-       #
-       #Run the game
-       spinner.run()
-       
-       #
-       #Game ending, so quit
-       gtk.quit()
+if __name__ == "__main__":
+    #
+    #Read in the game file how can you tell?
+    values = {}
+    print sys.argv
+    if(len(sys.argv) > 1):
+        print "Loading game file"
+        execfile(sys.argv[1], globals(), values)
+    else :
+        raise Exception, "Can't run with out a game file"
+    
+    gamedata = values.get('gamedata')
+    evtmngr = EventManager()
+    gtk = GUIToolkit()
+    keybd = Input(evtmngr, gtk)
+    spinner = CPUSpinnerController(evtmngr)
+    te = TileEngine(evtmngr, gamedata, gtk)
+    #game = Game( evManager )
+    
+    #
+    #Run the game
+    spinner.run()
+    
+    #
+    #Game ending, so quit
+    gtk.quit()
