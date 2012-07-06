94bb69c7c7ea870280fec5a2ee6219aa4f42870f
Nick Loadholtes <nick@ironboundsoftware.com>
2012-07-05 22:13:19 -0400

    Adding in party generation code

--- tests/encounter_tester.py
+++ tests/encounter_tester.py
@@ -7,9 +7,19 @@
 #
 import EncounterManager as em
 import Encounter
+from Character import Character as pc
+
+def generateParty(num=2):
+    output = []
+    for x in xrange(num):
+        c = pc(None, "Player_%s" % x, [0,0], None, None, None)
+        output.append(c)
+    return output
 
 
 if __name__ == "__main__":
+    goodguys = generateParty(1)
+    badguys = generateParty(1)
     encmgr = em.EncounterEngine()
     encmgr.startEncounter(Encounter.encounter)
 
