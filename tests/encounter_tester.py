91611980de649ee91c0eddadb003ba15551f6fbb
Nick Loadholtes <nick@ironboundsoftware.com>
2012-07-05 21:29:51 -0400

    Removing some commented out code

--- tests/encounter_tester.py
+++ tests/encounter_tester.py
@@ -5,28 +5,14 @@
 #
 # A tester to try out non-fighting encounters
 #
-from EncounterManager import *
-
-#def startenc(encounter):
-#    print "Starting encounter:"
-#    print 'Preamble: ', encounter['preamble']
-#    x = 1
-#    while(x):
-#        topic = encounter['conversation'][x]
-#        print "\t",topic[0]
-#        if(len(topic) > 1):
-#            x = eval(topic[1])
-        
+import EncounterManager as em
 
 
 if __name__ == "__main__":
     encounters = {}
-    execfile('../src/encounter.py', globals(), encounters)
     print encounters['encounter']['title']
-    encmgr = EncounterEngine()
+    encmgr = em.EncounterEngine()
     encmgr.startEncounter(encounters['encounter'])
-#    target = raw_input()
-#    if target == '1':
 
 
 
