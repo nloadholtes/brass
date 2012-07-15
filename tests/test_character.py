d262ddb1f453abfa398fc7f079a4ed6f50bca5ad
Nick Loadholtes <nick@ironboundsoftware.com>
2012-07-14 22:29:52 -0400

    Refactored the test to reuse helper functions

--- tests/test_character.py
+++ tests/test_character.py
@@ -10,23 +10,28 @@ from EventMngr import *
 def testBadInit():
     c = C()
 
+def getChar():
+    return  C(None, "test", (0,0), None, None, None)
+
+def getItem():
+    mngr = EventManager()
+    i = Item(mngr)
+    return i
+
 def testInit():
-    c = C(None, "test", (0,0), None, None, None)
+    c = getChar()
     assert c
 
 def testpickup():
-    c = C(None, "test", (0,0), None, None, None)
+    c = getChar()
     assert len(c.inventory) == 0
-    mngr = EventManager()
-    i = Item(mngr)
-    c.pickup(i)
+    c.pickup(getItem())
     assert len(c.inventory) == 1
 
 def testdrop():
-    c = C(None, "test", (0,0), None, None, None)
+    c = getChar()
     assert len(c.inventory) == 0
-    mngr = EventManager()
-    i = Item(mngr)
+    i = getItem()
     c.pickup(i)
     c.drop(i)
     assert len(c.inventory) == 0
