e1065b008ea10421f66156b221713a21bccc2a4a
Nick Loadholtes <nick@ironboundsoftware.com>
2012-07-14 22:23:17 -0400

    Testing out pickup, which seemed to be working already

--- tests/test_character.py
+++ tests/test_character.py
@@ -3,6 +3,8 @@
 
 from nose.tools import raises
 from Character import Character as C
+from Entity import Item
+from EventMngr import *
 
 @raises(Exception)
 def testBadInit():
@@ -11,3 +13,11 @@ def testBadInit():
 def testInit():
     c = C(None, "test", (0,0), None, None, None)
     assert c
+
+def testpickup():
+    c = C(None, "test", (0,0), None, None, None)
+    assert len(c.inventory) == 0
+    mngr = EventManager()
+    i = Item(mngr)
+    c.pickup(i)
+    assert len(c.inventory) == 1
