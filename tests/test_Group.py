9974007afbd90368dc22826206af6febad9800a3
Nick Loadholtes <nick@ironboundsoftware.com>
2012-07-16 21:44:03 -0400

    Generated a unit test for groups

---
+++ tests/test_Group.py
@@ -0,0 +1,18 @@
+from nose import SkipTest
+from nose.tools import assert_equal
+
+class TestGroup:
+    def test___init__(self):
+        # group = Group(manager, name, position, imageFileName)
+        raise SkipTest # TODO: implement your test here
+
+    def test_addSingle(self):
+        # group = Group(manager, name, position, imageFileName)
+        # assert_equal(expected, group.addSingle(who))
+        raise SkipTest # TODO: implement your test here
+
+    def test_populateClones(self):
+        # group = Group(manager, name, position, imageFileName)
+        # assert_equal(expected, group.populateClones(who, numberof))
+        raise SkipTest # TODO: implement your test here
+
