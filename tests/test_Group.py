b2632b74dfe877a6b6a4eb6623b66f7b36dd1691
Nick Loadholtes <nick@ironboundsoftware.com>
2012-07-17 22:07:27 -0400

    Making these test live, but the last one fails for reasons it shouldn't

--- tests/test_Group.py
+++ tests/test_Group.py
@@ -1,18 +1,26 @@
 from nose import SkipTest
 from nose.tools import assert_equal
+from Group import Group
+
+manager = None
+name = None
+position = None
+imageFileName = None
 
 class TestGroup:
     def test___init__(self):
-        # group = Group(manager, name, position, imageFileName)
-        raise SkipTest # TODO: implement your test here
+        group = Group(None, None, None, None)
+        assert group is not None
 
     def test_addSingle(self):
-        # group = Group(manager, name, position, imageFileName)
-        # assert_equal(expected, group.addSingle(who))
-        raise SkipTest # TODO: implement your test here
+        group = Group(manager, name, position, imageFileName)
+        assert_equal(0, len(group.characters))
+        group.addSingle({})
+        assert_equal(1, len(group.characters))
 
     def test_populateClones(self):
-        # group = Group(manager, name, position, imageFileName)
-        # assert_equal(expected, group.populateClones(who, numberof))
-        raise SkipTest # TODO: implement your test here
-
+        group2 = Group(manager, name, position, imageFileName)
+        # import pdb; pdb.set_trace()
+        assert_equal(0, len(group2.characters))
+        group2.populateClones({}, 20)
+        assert_equal(20, len(group2.characters))
