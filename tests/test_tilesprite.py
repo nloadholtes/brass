bcdfe363b570d0d11e3688f129cb4b20b46b3215
Nick Loadholtes <nick@ironboundsoftware.com>
2012-07-17 22:32:06 -0400

    Generated unit tests to be fleshed out later

---
+++ tests/test_tilesprite.py
@@ -0,0 +1,39 @@
+import unittest
+from nose import SkipTest
+from nose.tools import assert_equal
+
+class TestTileSprite:
+    def test___init__(self):
+        # tile_sprite = TileSprite(manager, imageFilename, parent, x, y, gtk)
+        raise SkipTest # TODO: implement your test here
+
+    def test_getXY(self):
+        # tile_sprite = TileSprite(manager, imageFilename, parent, x, y, gtk)
+        # assert_equal(expected, tile_sprite.getXY())
+        raise SkipTest # TODO: implement your test here
+
+    def test_handle(self):
+        # tile_sprite = TileSprite(manager, imageFilename, parent, x, y, gtk)
+        # assert_equal(expected, tile_sprite.handle())
+        raise SkipTest # TODO: implement your test here
+
+    def test_move(self):
+        # tile_sprite = TileSprite(manager, imageFilename, parent, x, y, gtk)
+        # assert_equal(expected, tile_sprite.move(direction))
+        raise SkipTest # TODO: implement your test here
+
+    def test_occupied(self):
+        # tile_sprite = TileSprite(manager, imageFilename, parent, x, y, gtk)
+        # assert_equal(expected, tile_sprite.occupied(intruder))
+        raise SkipTest # TODO: implement your test here
+
+    def test_paint(self):
+        # tile_sprite = TileSprite(manager, imageFilename, parent, x, y, gtk)
+        # assert_equal(expected, tile_sprite.paint(screen, location))
+        raise SkipTest # TODO: implement your test here
+
+    def test_printm(self):
+        # tile_sprite = TileSprite(manager, imageFilename, parent, x, y, gtk)
+        # assert_equal(expected, tile_sprite.printm(text))
+        raise SkipTest # TODO: implement your test here
+
