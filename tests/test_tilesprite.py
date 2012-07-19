38727f49c674fb46d6634592e24f641d233516fa
Nick Loadholtes <nick@ironboundsoftware.com>
2012-07-18 20:53:09 -0400

    Real tests

--- tests/test_tilesprite.py
+++ tests/test_tilesprite.py
@@ -1,16 +1,24 @@
 import unittest
 from nose import SkipTest
-from nose.tools import assert_equal
+from nose.tools import assert_equal, assert_is_not_none
+
+from tilesprite import TileSprite
+
+manager = None
+imageFilename = ""
+parent = None
+x = 0
+y = 0
+gtk = None
 
 class TestTileSprite:
     def test___init__(self):
-        # tile_sprite = TileSprite(manager, imageFilename, parent, x, y, gtk)
-        raise SkipTest # TODO: implement your test here
+        tile_sprite = TileSprite(manager, imageFilename, parent, x, y, gtk)
+        assert_is_not_none(tile_sprite)
 
     def test_getXY(self):
-        # tile_sprite = TileSprite(manager, imageFilename, parent, x, y, gtk)
-        # assert_equal(expected, tile_sprite.getXY())
-        raise SkipTest # TODO: implement your test here
+        tile_sprite = TileSprite(manager, imageFilename, parent, x, y, gtk)
+        assert_equal((x,y), tile_sprite.getXY())
 
     def test_handle(self):
         # tile_sprite = TileSprite(manager, imageFilename, parent, x, y, gtk)
