57f2f5923fa9f1fbe0b2e565c6f0d23c68fd4d6f
Nick Loadholtes <nick@ironboundsoftware.com>
2012-07-22 22:42:40 -0400

    Changing this test to use the nose stuff instead of failing outright.

--- tests/test_tileengine.py
+++ tests/test_tileengine.py
@@ -1,5 +1,5 @@
+from nose import SkipTest
 from nose.tools import assert_equals, assert_is_not_none
-import unittest
 from mock import MagicMock as Mock
 
 from tileengine import TileEngine
@@ -9,86 +9,83 @@ gamedata = Mock()
 eventmanager.registerObserver = Mock()
 gamedata.get = Mock(return_value=(0,0))
 
-class TestTileEngine(unittest.TestCase):
+class TestTileEngine:
     def test___init__(self):
         tile_engine = TileEngine(eventmanager, gamedata)
         assert_is_not_none(tile_engine)
 
-
     def test_addPlayer(self):
         # tile_engine = TileEngine(eventmanager, gamedata)
-        # self.assertEqual(expected, tile_engine.addPlayer(image, startpos, stats))
-        assert False # TODO: implement your test here
+        # assert_equal(expected, tile_engine.addPlayer(image, startpos, stats))
+        raise SkipTest # TODO: implement your test here
 
     def test_centerOn(self):
         # tile_engine = TileEngine(eventmanager, gamedata)
-        # self.assertEqual(expected, tile_engine.centerOn(sprite))
-        assert False # TODO: implement your test here
+        # assert_equal(expected, tile_engine.centerOn(sprite))
+        raise SkipTest # TODO: implement your test here
 
     def test_encounterHandler(self):
         # tile_engine = TileEngine(eventmanager, gamedata)
-        # self.assertEqual(expected, tile_engine.encounterHandler(evt))
-        assert False # TODO: implement your test here
+        # assert_equal(expected, tile_engine.encounterHandler(evt))
+        raise SkipTest # TODO: implement your test here
 
     def test_getImage(self):
         # tile_engine = TileEngine(eventmanager, gamedata)
-        # self.assertEqual(expected, tile_engine.getImage(imagename))
-        assert False # TODO: implement your test here
+        # assert_equal(expected, tile_engine.getImage(imagename))
+        raise SkipTest # TODO: implement your test here
 
     def test_initTE(self):
         # tile_engine = TileEngine(eventmanager, gamedata)
-        # self.assertEqual(expected, tile_engine.initTE(gtk))
-        assert False # TODO: implement your test here
+        # assert_equal(expected, tile_engine.initTE(gtk))
+        raise SkipTest # TODO: implement your test here
 
     def test_loadMap(self):
         # tile_engine = TileEngine(eventmanager, gamedata)
-        # self.assertEqual(expected, tile_engine.loadMap(mapname))
-        assert False # TODO: implement your test here
+        # assert_equal(expected, tile_engine.loadMap(mapname))
+        raise SkipTest # TODO: implement your test here
 
     def test_loadSprites(self):
         # tile_engine = TileEngine(eventmanager, gamedata)
-        # self.assertEqual(expected, tile_engine.loadSprites(spritelist))
-        assert False # TODO: implement your test here
+        # assert_equal(expected, tile_engine.loadSprites(spritelist))
+        raise SkipTest # TODO: implement your test here
 
     def test_move(self):
         # tile_engine = TileEngine(eventmanager, gamedata)
-        # self.assertEqual(expected, tile_engine.move(direction))
-        assert False # TODO: implement your test here
+        # assert_equal(expected, tile_engine.move(direction))
+        raise SkipTest # TODO: implement your test here
 
     def test_moveOk(self):
         # tile_engine = TileEngine(eventmanager, gamedata)
-        # self.assertEqual(expected, tile_engine.moveOk(newx, newy))
-        assert False # TODO: implement your test here
+        # assert_equal(expected, tile_engine.moveOk(newx, newy))
+        raise SkipTest # TODO: implement your test here
 
     def test_moveToNewRoom(self):
         # tile_engine = TileEngine(eventmanager, gamedata)
-        # self.assertEqual(expected, tile_engine.moveToNewRoom(door))
-        assert False # TODO: implement your test here
+        # assert_equal(expected, tile_engine.moveToNewRoom(door))
+        raise SkipTest # TODO: implement your test here
 
     def test_notify(self):
         # tile_engine = TileEngine(eventmanager, gamedata)
-        # self.assertEqual(expected, tile_engine.notify(evt))
-        assert False # TODO: implement your test here
+        # assert_equal(expected, tile_engine.notify(evt))
+        raise SkipTest # TODO: implement your test here
 
     def test_paint(self):
         # tile_engine = TileEngine(eventmanager, gamedata)
-        # self.assertEqual(expected, tile_engine.paint())
-        assert False # TODO: implement your test here
+        # assert_equal(expected, tile_engine.paint())
+        raise SkipTest # TODO: implement your test here
 
     def test_paintSprite(self):
         # tile_engine = TileEngine(eventmanager, gamedata)
-        # self.assertEqual(expected, tile_engine.paintSprite(sprite))
-        assert False # TODO: implement your test here
+        # assert_equal(expected, tile_engine.paintSprite(sprite))
+        raise SkipTest # TODO: implement your test here
 
     def test_removeSprite(self):
         # tile_engine = TileEngine(eventmanager, gamedata)
-        # self.assertEqual(expected, tile_engine.removeSprite(sprite))
-        assert False # TODO: implement your test here
+        # assert_equal(expected, tile_engine.removeSprite(sprite))
+        raise SkipTest # TODO: implement your test here
 
     def test_setMessage(self):
         # tile_engine = TileEngine(eventmanager, gamedata)
-        # self.assertEqual(expected, tile_engine.setMessage(messagestr))
-        assert False # TODO: implement your test here
+        # assert_equal(expected, tile_engine.setMessage(messagestr))
+        raise SkipTest # TODO: implement your test here
 
-if __name__ == '__main__':
-    unittest.main()
