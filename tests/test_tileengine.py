116ed2145a0766e19f92d13639d76f873b187b33
Nick Loadholtes <nick@ironboundsoftware.com>
2012-07-22 21:13:18 -0400

    Thanks to pythoscope, I now have 20 new test cases to implement.

---
+++ tests/test_tileengine.py
@@ -0,0 +1,84 @@
+import unittest
+
+class TestTileEngine(unittest.TestCase):
+    def test___init__(self):
+        # tile_engine = TileEngine(eventmanager, gamedata)
+        assert False # TODO: implement your test here
+
+    def test_addPlayer(self):
+        # tile_engine = TileEngine(eventmanager, gamedata)
+        # self.assertEqual(expected, tile_engine.addPlayer(image, startpos, stats))
+        assert False # TODO: implement your test here
+
+    def test_centerOn(self):
+        # tile_engine = TileEngine(eventmanager, gamedata)
+        # self.assertEqual(expected, tile_engine.centerOn(sprite))
+        assert False # TODO: implement your test here
+
+    def test_encounterHandler(self):
+        # tile_engine = TileEngine(eventmanager, gamedata)
+        # self.assertEqual(expected, tile_engine.encounterHandler(evt))
+        assert False # TODO: implement your test here
+
+    def test_getImage(self):
+        # tile_engine = TileEngine(eventmanager, gamedata)
+        # self.assertEqual(expected, tile_engine.getImage(imagename))
+        assert False # TODO: implement your test here
+
+    def test_initTE(self):
+        # tile_engine = TileEngine(eventmanager, gamedata)
+        # self.assertEqual(expected, tile_engine.initTE(gtk))
+        assert False # TODO: implement your test here
+
+    def test_loadMap(self):
+        # tile_engine = TileEngine(eventmanager, gamedata)
+        # self.assertEqual(expected, tile_engine.loadMap(mapname))
+        assert False # TODO: implement your test here
+
+    def test_loadSprites(self):
+        # tile_engine = TileEngine(eventmanager, gamedata)
+        # self.assertEqual(expected, tile_engine.loadSprites(spritelist))
+        assert False # TODO: implement your test here
+
+    def test_move(self):
+        # tile_engine = TileEngine(eventmanager, gamedata)
+        # self.assertEqual(expected, tile_engine.move(direction))
+        assert False # TODO: implement your test here
+
+    def test_moveOk(self):
+        # tile_engine = TileEngine(eventmanager, gamedata)
+        # self.assertEqual(expected, tile_engine.moveOk(newx, newy))
+        assert False # TODO: implement your test here
+
+    def test_moveToNewRoom(self):
+        # tile_engine = TileEngine(eventmanager, gamedata)
+        # self.assertEqual(expected, tile_engine.moveToNewRoom(door))
+        assert False # TODO: implement your test here
+
+    def test_notify(self):
+        # tile_engine = TileEngine(eventmanager, gamedata)
+        # self.assertEqual(expected, tile_engine.notify(evt))
+        assert False # TODO: implement your test here
+
+    def test_paint(self):
+        # tile_engine = TileEngine(eventmanager, gamedata)
+        # self.assertEqual(expected, tile_engine.paint())
+        assert False # TODO: implement your test here
+
+    def test_paintSprite(self):
+        # tile_engine = TileEngine(eventmanager, gamedata)
+        # self.assertEqual(expected, tile_engine.paintSprite(sprite))
+        assert False # TODO: implement your test here
+
+    def test_removeSprite(self):
+        # tile_engine = TileEngine(eventmanager, gamedata)
+        # self.assertEqual(expected, tile_engine.removeSprite(sprite))
+        assert False # TODO: implement your test here
+
+    def test_setMessage(self):
+        # tile_engine = TileEngine(eventmanager, gamedata)
+        # self.assertEqual(expected, tile_engine.setMessage(messagestr))
+        assert False # TODO: implement your test here
+
+if __name__ == '__main__':
+    unittest.main()
