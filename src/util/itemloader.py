ed9c71002ca4d2d4790c1da7129cf40d42f51526
Nick Loadholtes <nick@ironboundsoftware.com>
2013-04-20 22:53:04 -0400

    Out with the dead. Especially the XML dead.

--- src/util/itemloader.py
+++ src/util/itemloader.py
@@ -2,38 +2,12 @@
 # itemloader.py
 #
 # A loader for the items
-from xml.dom import *
-import xml.dom.minidom as dom
 import json
 
 ITEM_FILE = 'data/items.json'
 
 def loadItemsFromJson(filename):
     return json.loads(filename)
-
-def loadItemsFromXML(filename):
-    from Entity import Item
-    doc = dom.parse(filename)
-    itemlist = []
-    print 'doc is ', doc
-    for items in doc.childNodes:
-        for itemx in items.childNodes:
-            if itemx.nodeType == Node.ELEMENT_NODE:
-                thing = Item()
-                for item in itemx.childNodes:
-                    if item.nodeName != '#text':
-                        thing = createObjectFromXML(thing, item)
-                print thing
-                itemlist.append(thing)
-    return itemlist
-
-def createObjectFromXML(obj, nodes):
-    '''Creates an object from a xml document'''
-    name = nodes.localName
-    for node in nodes.childNodes:
-        #print name, " - ", node.nodeValue
-        setattr(obj, str(node.parentNode.localName), node.nodeValue)
-    return obj
 
 def addItem():
     i = {}
